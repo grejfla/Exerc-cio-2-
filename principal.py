16de5b222ca0cc2ddef04d1a5f05fe2847161a1f
grejfla <grejfla@gmail.com>
2026-05-15 20:37:02 -0300

    reproduzido o código

---
+++ principal.py
@@ -0,0 +1,4 @@
+delta_s = float (input ("Digite o deslocamento (em metros): "))
+delta_t = float (input("Digite o tempo  (em segundo): "))
+velocidade = delta_s / delta_t
+print(f"vm= {velocidade: .2f} m/s")
